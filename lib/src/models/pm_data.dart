488e39e07c18056fa12801c838c128b1c0fbadd8
william <wmmnpr@gmail.com>
2024-06-25 23:59:36 +0200

    add logging

--- lib/src/models/pm_data.dart
+++ lib/src/models/pm_data.dart
@@ -1,8 +1,13 @@
 typedef IntList = List<int>;
 
 abstract class PmData<T> {
-  T from(IntList intList) { throw UnimplementedError();}
-  IntList to(IntList command) { throw UnimplementedError();}
+  T from(IntList intList) {
+    throw UnimplementedError();
+  }
+
+  IntList to(IntList command) {
+    throw UnimplementedError();
+  }
 
   Map<String, dynamic> toJson();
 }
--- lib/src/pm_device.dart
+++ lib/src/pm_device.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:ergc2_pm_csafe/src/models/data_conv_utils.dart';
 import 'package:ergc2_pm_csafe/src/models/pm_data.dart';
+import 'package:logging/logging.dart';
 
 abstract class PmBleCharacteristic<T> {
   void listen(StreamSink sink) {
@@ -22,19 +23,24 @@ abstract class PmBleCharacteristic<T> {
 }
 
 class PmBLEDevice {
+  final log = Logger('PmBLEDevice');
+
   Map<int, PmBleCharacteristic> characteristics;
 
   PmBLEDevice(this.characteristics);
 
   Stream<T> subscribe<T extends PmData<T>>(int uuid) {
+    log.info('subscribing to characteristic: $uuid');
     StreamController<IntList> deviceStream = StreamController<IntList>();
     StreamController<T> clientStream = StreamController<T>();
     characteristics[uuid]!.listen(deviceStream.sink);
     deviceStream.stream.listen((event) {
       clientStream.add(characteristics[uuid]!.create().from(event));
     }, onDone: () {
+      log.info('closing characteristic: $uuid');
       clientStream.close();
     }, onError: (err) {
+      log.warning('error characteristic: $uuid: $err');
       clientStream.addError(err);
     });
     return clientStream.stream;
@@ -42,12 +48,16 @@ class PmBLEDevice {
 
   Future<void> sendCommand(int uuid, IntList command,
       [String message = "NONE"]) async {
-    print(message);
     IntList csafeBuffer = DataConvUtils.toCsafe(command);
+    if (log.isLoggable(Level.ALL)) {
+      log.info(
+          'sending csafe command: ${DataConvUtils.intArrayToHex(csafeBuffer)}');
+    }
     return characteristics[uuid]!.writeCsafe(csafeBuffer);
   }
 
   Future<IntList> readCharacteristic(int uuid) async {
+    log.info('reading characteristic: $uuid');
     return characteristics[uuid]!.readCsafe();
   }
 }
